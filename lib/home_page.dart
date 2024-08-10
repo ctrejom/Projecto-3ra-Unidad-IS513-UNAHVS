df4d5e137a471d067a5e150ddf26620c83880869
trejomejia13 <trejomejia13@gmail.com>
2024-08-10 23:22:28 +0000

    Botones de Categoria, Decoracion de Containers para Articulos y Navigation Bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) { //Funcion para el navigation bar
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,35 +40,68 @@ class HomePage extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.grey,
-                      child: Icon(Icons.woman_outlined),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        print('Categoría: Ropa de Hombre');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[200],
+                        foregroundColor: Colors.black,
+                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      icon: Icon(Icons.male, size: 30, color: Colors.blue),
+                      label: Text('Ropa de Hombre'),
                     ),
                     SizedBox(width: 10),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.grey,
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        print('Categoría: Joyeria');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[200],
+                        foregroundColor: Colors.black,
+                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      icon: Icon(Icons.watch, size: 30, color: Colors.purple),
+                      label: Text('Joyeria'),
                     ),
                     SizedBox(width: 10),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.grey,
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        print('Categoría: Electrónicos');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[200],
+                        foregroundColor: Colors.black,
+                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      icon: Icon(Icons.electrical_services, size: 30, color: Colors.orange),
+                      label: Text('Electrónicos'),
                     ),
                     SizedBox(width: 10),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.grey,
-                    ),
-                    SizedBox(width: 10),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.grey,
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        print('Categoría: Ropa de Mujer');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[200],
+                        foregroundColor: Colors.black,
+                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      icon: Icon(Icons.female, size: 30, color: Colors.pink),
+                      label: Text('Ropa de Mujer'),
                     ),
                   ],
                 ),
@@ -76,36 +122,116 @@ class HomePage extends StatelessWidget {
                     Container(
                       width: 100,
                       height: 100,
-                      color: Colors.blue,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                       child: Center(child: Text('Item 1')),
                     ),
                     SizedBox(width: 10),
                     Container(
                       width: 100,
                       height: 100,
-                      color: Colors.blue,
-                      child: Center(child: Text('Item 2')),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text('Item 1')),
                     ),
                     SizedBox(width: 10),
                     Container(
                       width: 100,
                       height: 100,
-                      color: Colors.blue,
-                      child: Center(child: Text('Item 3')),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text('Item 1')),
                     ),
                     SizedBox(width: 10),
                     Container(
                       width: 100,
                       height: 100,
-                      color: Colors.blue,
-                      child: Center(child: Text('Item 4')),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text('Item 1')),
                     ),
                     SizedBox(width: 10),
                     Container(
                       width: 100,
                       height: 100,
-                      color: Colors.blue,
-                      child: Center(child: Text('Item 5')),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text('Item 1')),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Best For You UwU',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text('Item 1')),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text('Item 1')),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text('Item 1')),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text('Item 1')),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text('Item 1')),
                     ),
                   ],
                 ),
@@ -113,6 +239,33 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold), 
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal), 
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Mis Pedidos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Carrito',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.payment),
+            label: 'Facturación',
+          ),
+        ],
       ),
     );
   }
