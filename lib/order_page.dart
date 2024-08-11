fd47ffda92762396eced80f79a9161dd17c64e13
trejomejia13 <trejomejia13@gmail.com>
2024-08-11 02:39:36 +0000

    Creacion de OrderPage e implemetacion del WidgetCustom

---
+++ lib/order_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'Widgets/NavigationBarCustom.dart';
+
+class OrderPage extends StatefulWidget {
+  OrderPage({super.key});
+
+  @override
+  _OrderPageState createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  int _selectedIndex = 1;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      switch (index) {
+        case 0:
+          Navigator.pushNamed(context, 'Home');
+          break;
+        case 1:
+          Navigator.pushNamed(context, 'Order');
+          break;
+        case 2:
+          Navigator.pushNamed(context, 'Cart');
+          break;
+        case 3:
+          Navigator.pushNamed(context, 'Payment');
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('OrderPage'),
+      ),
+      body: Center(
+        child: Text('This is the Order Page'),
+      ),
+      bottomNavigationBar: NavigationBarCustom(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
+      ),
+    );
+  }
+}
